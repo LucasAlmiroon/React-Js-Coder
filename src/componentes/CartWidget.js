0d293db0bed68ca93de3b821de2da39947280e25
LucasAlmiron <almiron736@gmail.com>
2023-09-14 16:16:18 -0300

    Correccion en estilos

--- src/componentes/CartWidget.js
+++ src/componentes/CartWidget.js
@@ -3,7 +3,7 @@ import { faB, faCartShopping } from '@fortawesome/free-solid-svg-icons';
 
 export default function CartWidget() {
     return (
-        <li>
+        <li className='btn-navBar'>
             <FontAwesomeIcon icon={faCartShopping} />
             <label>1</label>
         </li>
